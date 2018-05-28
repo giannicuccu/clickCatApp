5ec5e6f3258e9f663846e193f76c3f2669d792ea
Gianni Cuccu <gianni.cuccu@gmail.com>
2018-05-29 01:37:19 +0200

    Completed required functionalities

--- js/app.js
+++ js/app.js
@@ -1,15 +1,63 @@
- let myCats = ['micio','zoi','spike'];
+ let myCats = ['micio','zoi','spike','tex','gatto'];
 
   const CatApp = ( function(appCats){
 
     const cats = [];
     console.log(this);
+
+
+    const CatAppController = function(){
+        console.log(cats);
+        // this.cats = cats;
+        this.initSidebar = function(){
+            console.log(this);
+            let parentEl = document.getElementById('catlist');
+            cats.forEach((cat) => {
+                let listEl = document.createElement("LI");
+                let listElLink = document.createElement("A");
+                    listElLink.setAttribute("data-id", cat.id);
+                    listElLink.textContent = cat.name+' - ';
+                    listElLink.addEventListener('click',(e)=>{cat.updateClick()})
+                let listElLinkSpan = document.createElement("SPAN");
+                    listElLinkSpan.textContent = cat.clickTotal;
+                    listElLink.appendChild(listElLinkSpan);
+                listEl.appendChild(listElLink);
+                parentEl.appendChild(listEl);
+
+                cat.sideTotal = listElLinkSpan;
+                
+            });
+            
+            }
+
+            this.showCat = function(cat){
+                console.log('show'+cat)
+                let parentEl = document.getElementById('main');
+                parentEl.innerHTML = '';
+                let div = document.createElement("DIV"); 
+                div.setAttribute('ID','cat-'+cat.id);
+                div.setAttribute('class','cat');
+                let img = document.createElement("IMG");
+                img.setAttribute("SRC",'/img/'+cat.name+'.jpg');
+                img.setAttribute("data-id",this.id);                
+                div.appendChild(img);
+                let counter = document.createElement('p');
+                 counter.textContent = cat.name+' - '+cat.clickTotal;
+                 div.appendChild(counter);
+                 parentEl.appendChild(div);
+                }
+            
+        }
+    
+
+
     const Cat = function (name,i) {
         this.id = i;
         this.name = name;
         this.pic = name;
         this.clickTotal = 0;
-        this.addToDOM();
+        this.sideTotal = undefined;
+        //this.addToDOM();
 
     }
 
@@ -34,7 +82,8 @@
 
     Cat.prototype.updateClick = function(){
         this.clickTotal++;
-        this.counter.innerText = this.clickTotal;
+        this.sideTotal.innerText = this.clickTotal;
+        controller.showCat(this)
     }
 
 
@@ -44,6 +93,8 @@
             cats.push(cat);          
         });
 
+        
+
         // cats.forEach((cat) =>{
         //     console.log(this)
         //     document.getElementById(`${cat.name}-${cat.id}`).addEventListener('click',function(e){
@@ -51,7 +102,10 @@
         //  },false)
         // })
     }
-
+    
     init(appCats);
+    let controller = new CatAppController(cats);
+    controller.initSidebar();
+    
     
 })(myCats);
