f96088a5ac881b457700a11e2bba88115c61fc0a
Gianni Cuccu <gianni.cuccu@gmail.com>
2018-06-02 10:24:19 +0200

    REFACTORED as MVO

--- js/app.js
+++ js/app.js
@@ -1,54 +1,118 @@
  let myCats = ['micio','zoi','spike','tex','gatto'];
 
-  const CatApp = ( function(appCats){
+  const CatApp = (function(appCats){
+      
 
-    const cats = [];
-    console.log(this);
+    const model = {
 
+        cats: [],
+        activeCat:{},
 
-    const CatAppController = function(){
-        console.log(cats);
-        // this.cats = cats;
-        this.initSidebar = function(){
-            console.log(this);
-            let parentEl = document.getElementById('catlist');
-            cats.forEach((cat) => {
-                let listEl = document.createElement("LI");
-                let listElLink = document.createElement("A");
-                    listElLink.setAttribute("data-id", cat.id);
-                    listElLink.textContent = cat.name+' - ';
-                    listElLink.addEventListener('click',(e)=>{cat.updateClick()})
-                let listElLinkSpan = document.createElement("SPAN");
-                    listElLinkSpan.textContent = cat.clickTotal;
-                    listElLink.appendChild(listElLinkSpan);
-                listEl.appendChild(listElLink);
-                parentEl.appendChild(listEl);
-
-                cat.sideTotal = listElLinkSpan;
-                
+        init: (catNames) => {
+            catNames.forEach((catName,i) => {            
+                let cat =  new Cat(catName,i);
+                model.cats.push(cat);          
             });
-            
+        },
+
+        setActiveCat: (cat) => {
+            model.activeCat = {};
+            model.activeCat = cat;
+        },
+
+        getActiveCat: () => {
+            return model.activeCat;           
+        },
+
+        saveModel: ()=>{
+
+        },
+
+        loadModel: () =>{
+
+        }
+    }
+
+    const octopus = {
+
+        initModel: (catNames) => {
+            model.init(catNames)
+        },
+
+        initSidebar: () => {
+            catList = model.cats;
+            view.sidebar.initSidebar(catList)
+        },
+
+        initCatViewer: () => {
+            view.catViewer.initCatViewer();
+        },
+
+        showCat: (cat) => {
+            model.setActiveCat(cat);
+            view.catViewer.showCat(model.getActiveCat());
+        },
+
+        addClick: (cat) => {
+            cat.updateClick();
+            view.catViewer.showCat(model.getActiveCat());            
+        }
+
+    }
+
+    const view = {
+
+        sidebar: {
+            initSidebar: (cats) => {
+               
+                let parentEl = document.getElementById('catlist');
+                cats.forEach((cat) => {
+                    let listEl = document.createElement("LI");
+                    let listElLink = document.createElement("A");
+                        listElLink.setAttribute("data-id", cat.id);
+                        listElLink.textContent = cat.name+' - ';
+                        listElLink.addEventListener('click',(e)=>{octopus.showCat(cat)});
+                    let listElLinkSpan = document.createElement("SPAN");
+                        listElLinkSpan.textContent = cat.clickTotal;
+                        listElLink.appendChild(listElLinkSpan);
+                    listEl.appendChild(listElLink);
+                    parentEl.appendChild(listEl);
+
+                    // INTERESTING - WORKS
+                    //cat.sideTotal = listElLinkSpan; // SAVE THE REFERENCE OF THE DOME ELEMENT AS A PROPERTY IN THE CAT OBJECT
+                    
+                });
+    
+            }
+        },
+
+        catViewer:{
+
+           initCatViewer: () => {
+               console.log('cat wiewer init')
+            },
+
+           showCat: (cat) => {
+            console.log(cat)
+            let parentEl = document.getElementById('main');
+            parentEl.innerHTML = ''; // CLEAN VIEWER AND REMOVE EVENT LISTENERS
+            let div = document.createElement("DIV"); 
+            div.setAttribute('ID','cat-'+cat.id);
+            div.setAttribute('class','cat');
+            let img = document.createElement("IMG");
+            img.setAttribute("SRC",'/img/'+cat.name+'.jpg');
+            img.setAttribute("data-id",this.id); 
+            img.addEventListener('click',(e)=>{octopus.addClick(cat)});               
+            div.appendChild(img);
+            let counter = document.createElement('p');
+             counter.textContent = cat.name+' - '+cat.clickTotal;
+             div.appendChild(counter);
+             parentEl.appendChild(div);
             }
 
-            this.showCat = function(cat){
-                console.log('show'+cat)
-                let parentEl = document.getElementById('main');
-                parentEl.innerHTML = '';
-                let div = document.createElement("DIV"); 
-                div.setAttribute('ID','cat-'+cat.id);
-                div.setAttribute('class','cat');
-                let img = document.createElement("IMG");
-                img.setAttribute("SRC",'/img/'+cat.name+'.jpg');
-                img.setAttribute("data-id",this.id);                
-                div.appendChild(img);
-                let counter = document.createElement('p');
-                 counter.textContent = cat.name+' - '+cat.clickTotal;
-                 div.appendChild(counter);
-                 parentEl.appendChild(div);
-                }
-            
-        }
-    
+        }        
+        
+    }
 
 
     const Cat = function (name,i) {
@@ -57,55 +121,19 @@
         this.pic = name;
         this.clickTotal = 0;
         this.sideTotal = undefined;
-        //this.addToDOM();
-
     }
-
-    Cat.prototype.addToDOM = function(){
-        let parentEl = document.getElementById('main');
-        let div = document.createElement("DIV"); 
-        div.setAttribute('ID','cat-'+this.id);
-        div.setAttribute('class','cat');
-            let img = document.createElement("IMG");
-            img.setAttribute("SRC",'/img/'+this.name+'.jpg');
-            img.setAttribute("data-id",this.id);
-            img.addEventListener('click',()=>{this.updateClick()})
-            div.appendChild(img);
-            let counter = document.createElement('p');
-            counter.textContent = '0';
-            div.appendChild(counter);
-    
-    parentEl.appendChild(div);
-    this.counter = counter;
-    }
-
 
     Cat.prototype.updateClick = function(){
         this.clickTotal++;
-        this.sideTotal.innerText = this.clickTotal;
-        controller.showCat(this)
+        // INTERESTING - WORKS
+        //this.sideTotal.innerText = this.clickTotal; // update the DOM element using the reference saved as cat object property
     }
 
 
-    let init = function(catNames){
-        catNames.forEach((catName,i) => {            
-            let cat =  new Cat(catName,i);
-            cats.push(cat);          
-        });
 
-        
+    octopus.initModel(appCats);
+    octopus.initSidebar();
+    octopus.initCatViewer();
 
-        // cats.forEach((cat) =>{
-        //     console.log(this)
-        //     document.getElementById(`${cat.name}-${cat.id}`).addEventListener('click',function(e){
-        //     cats[this.getAttribute('data-id')].updateClick();           
-        //  },false)
-        // })
-    }
-    
-    init(appCats);
-    let controller = new CatAppController(cats);
-    controller.initSidebar();
-    
-    
+
 })(myCats);
